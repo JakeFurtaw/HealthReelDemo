066d427ef2886ba6612250a1820379ebf494998c
JakeFurtaw <jfurtaw1@students.towson.edu>
2024-08-02 20:19:46 -0400

    updated utils, added memory and user storing

--- HealthG.py
+++ HealthG.py
@@ -1,22 +1,18 @@
 from utils import (
-    set_llm, setup_index_and_chat_engine, set_embed_model, load_docs
+    setup_index_and_chat_engine, load_environment_and_models
 )
 
 
 def main() -> None:
-    embed_model = set_embed_model()
-    llm = set_llm()
-    docs = load_docs()
-
+    embed_model, llm = load_environment_and_models()
     # Chat Engine, Added Memory
     chat_engine = setup_index_and_chat_engine(docs, embed_model, llm)
-
+    print("Welcome to HealthG, your personal health assistant.")
     while True:
         user_query = input("Ask Me Anything: ")
         if user_query.lower() == 'e':
             print("Thanks for using HealthG. Goodbye!")
             break
-
         response = chat_engine.chat(user_query)
         print("Response:", response)
         print("\n" + "-" * 100 + "\n")
--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 from llama_index.core.chat_engine.types import ChatMode
-from llama_index.core import VectorStoreIndex, Settings, Document
+from llama_index.embeddings.langchain import LangchainEmbedding
+from llama_index.core import VectorStoreIndex, Settings
 from llama_index.core.memory import ChatMemoryBuffer
 from llama_index.core.storage.chat_store import SimpleChatStore
 from llama_index.llms.ollama import Ollama
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 import torch
 
 
@@ -16,39 +17,52 @@ def set_device(gpu: int = None) -> str:
     return device
 
 
-def set_llm():
-    llm = Ollama(model="mistral-nemo:latest",
-                 request_timeout=30.0,
-                 device=set_device(0))
-    return llm
+def load_embedding_model(
+        model_name: str = "dunzhang/stella_en_1.5B_v5", device=set_device(0)
+) -> HuggingFaceBgeEmbeddings:
+    model_kwargs = {"device": device,
+                    "trust_remote_code": True}
+    encode_kwargs = {
+        "normalize_embeddings": True
+    }  # set True to compute cosine similarity
+    embedding_model = HuggingFaceBgeEmbeddings(
+        model_name=model_name,
+        model_kwargs=model_kwargs,
+        encode_kwargs=encode_kwargs,
+    )
+    return embedding_model
 
 
-def set_embed_model():
-    embed_model = HuggingFaceEmbedding(
-        model_name=r"C:\Programming\models\embedding\stella_en_400M_v5",
-        device=set_device(1),
-        trust_remote_code=True)
-    return embed_model
-
-
-# Saving user data to a json file
 def load_docs():
-    chat_store = SimpleChatStore.from_persist_path(persist_path="/data/chat_store.json")
-    documents = []
-    for message in chat_store.get_messages("user1"):  # Make sure "user1" matches the chat_store_key
-        documents.append(Document(text=message.content, metadata={"role": message.role}))
-    return documents
+    loaded_chat_store = SimpleChatStore.from_persist_path(
+        persist_path="data/chatstorage.json"
+    )
+    docs = loaded_chat_store
+    return docs
 
 
-def setup_index_and_chat_engine(documents, embed_model, llm):
-    # Memory by user, change chat store key to
+def load_environment_and_models():
+    lc_embedding_model = load_embedding_model()
+    embed_model = LangchainEmbedding(lc_embedding_model)
+    llm = Ollama(model="llama3.1:latest", request_timeout=30.0, device=set_device(1))
+    return embed_model, llm
+
+
+def setup_index_and_query_engine(docs, embed_model, llm):
+    index = VectorStoreIndex.from_documents(docs, embed_model=embed_model)
+    Settings.llm = llm
+    query_engine = index.as_query_engine(streaming=True, similarity_top_k=4)
+    return query_engine
+
+
+def setup_index_and_chat_engine(docs, embed_model, llm):
     chat_store = SimpleChatStore()
+    chat_store.persist(persist_path="data/chatstorage.json")
     memory = ChatMemoryBuffer.from_defaults(token_limit=3900,
                                             chat_store=chat_store,
                                             chat_store_key="user1")
-    chat_store.persist(persist_path="chat_store.json")
+    index = VectorStoreIndex.from_documents(docs, embed_model=embed_model)
     Settings.llm = llm
-    index = VectorStoreIndex.from_documents(documents, embed_model=embed_model)
     chat_engine = index.as_chat_engine(
         chat_mode=ChatMode.BEST,
         memory=memory,
